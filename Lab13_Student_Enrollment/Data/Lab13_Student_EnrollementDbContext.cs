5411f6279f31ef0666dc06a50f5b71482f1b6bc3
Anthony Green <Anthonydgreen90@gmail.com>
2018-06-25 09:32:47 -0700

    initial scaffolding

---
+++ Lab13_Student_Enrollment/Data/Lab13_Student_EnrollementDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab13_Student_Enrollment.Data
+{
+    public class Lab13_Student_EnrollementDbContext : DbContext
+    {
+        public Lab13_Student_EnrollementDbContext(DbContextOptions<Lab13_Student_EnrollementDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
